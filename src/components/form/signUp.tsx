47437d8b6298e5591648dacf656f0e2564541f49
Thinker-Dev <107056866+Thinker-Dev@users.noreply.github.com>
2024-06-06 22:44:57 +0200

    refresh token

--- src/components/form/signUp.tsx
+++ src/components/form/signUp.tsx
@@ -26,11 +26,54 @@ import { axiosInstance } from "@/lib/axios";
 import { toast } from "../ui/use-toast";
 
 export interface IUserResponse {
-  user: {
-    email: string;
-    id: string;
-  };
-  token: string;
+  profile: Profile;
+  session: Session;
+  user: User;
+}
+
+interface Session {
+  access_token: string;
+  token_type: string;
+  expires_in: number;
+  expires_at: number;
+  refresh_token: string;
+  user: User;
+}
+
+interface User {
+  id: string;
+  aud: string;
+  role: string;
+  email: string;
+  email_confirmed_at: string;
+  phone: string;
+  confirmed_at: string;
+  last_sign_in_at: string;
+  app_metadata: Appmetadata;
+  created_at: string;
+  updated_at: string;
+  is_anonymous: boolean;
+}
+
+interface Appmetadata {
+  provider: string;
+  providers: string[];
+}
+
+interface Profile {
+  id: number;
+  public_id: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  sex: string;
+  birt_date: string;
+  account_status: string;
+  nationality: string;
+  sport_id: number;
+  phone: null;
+  mobile: null;
+  address: string;
 }
 
 export const SignUpForm: FC = () => {
@@ -53,11 +96,12 @@ export const SignUpForm: FC = () => {
       .post<IUserResponse>("/auth/sign_up", values)
       .then((res) => {
         localStorage.setItem("user", JSON.stringify(res.data.user));
-        localStorage.setItem("token", res.data.token);
+        localStorage.setItem("profile", JSON.stringify(res.data.profile));
+        localStorage.setItem("session", JSON.stringify(res.data.session));
         router.push(`${lastSegment}/registration`);
         toast({
           title: "Sucesso",
-          description: res.data.token,
+          description: "",
           variant: "default",
         });
       })
--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,3 +1,4 @@
+import { IUserResponse } from "@/components/form/signUp";
 import axios from "axios";
 
 export const axiosInstance = axios.create({
@@ -6,13 +7,21 @@ export const axiosInstance = axios.create({
     "Content-Type": "application/json",
   },
 });
+interface Session {
+  access_token: string;
+  token_type: string;
+  expires_in: number;
+  expires_at: number;
+  refresh_token: string;
+
+}
 
 axiosInstance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("token");
-
+    const token = localStorage.getItem("session") as string;
+    const session: Session = JSON.parse(token);
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+      config.headers.Authorization = `Bearer ${session.access_token}`;
     }
     return config;
   },
@@ -20,3 +29,37 @@ axiosInstance.interceptors.request.use(
     return Promise.reject(error);
   }
 );
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      const access_token = await refreshAccessToken();
+      axiosInstance.defaults.headers.common["Authorization"] =
+        "Bearer " + access_token;
+      return axiosInstance(originalRequest);
+    }
+
+    return Promise.reject(error);
+  }
+);
+const refreshAccessToken = async () => {
+  try {
+    const tokens = localStorage.getString("session");
+    if (tokens) {
+      const token: Session = JSON.parse(tokens);
+
+      const keys = await axiosInstance.get<IUserResponse>("auth/refresh_token", {
+        headers: { Authorization: `Bearer ${token.refresh_token}` },
+      });
+      localStorage.set("session", JSON.stringify(keys.data.session));
+      return keys.data.session.access_token;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
