fc0b2ed432788c881e989abb84f4d9558563ebb9
Thinker-Dev <107056866+Thinker-Dev@users.noreply.github.com>
2024-08-06 23:25:16 +0200

    commit

--- src/components/subscriptionPlan/index.tsx
+++ src/components/subscriptionPlan/index.tsx
@@ -1,11 +1,28 @@
+"use client"
 import { subscriptionPlanData } from "@/data/subscriptionPlanData";
+import { privateInstance } from "@/lib/axios";
+import { useRouter } from "next/navigation";
 import React, { FC } from "react";
-
+interface IResponse {
+  session: string
+}
 export const SubscriptionPlan: FC = () => {
+  const router = useRouter()
+  const handleSubscription = async (plan_id: number) => {
+    try {
+      const response = await privateInstance.post<IResponse>('/checkout', { plan_id })
+      const session = response.data.session
+      router.push(session)
+    } catch (err) {
+      console.log(err);
+    }
+
+
+  }
   return (
     <div className="flex space-x-5">
       {subscriptionPlanData.map((item, index) => (
-        <div key={index} className="flex flex-col w-[300px] ">
+        <div key={index} className="flex flex-col w-[300px] " onClick={() => handleSubscription(index + 1)}>
           <div
             className={`transition-all hover:-translate-y-1 duration-300 cursor-pointer flex justify-center flex-col items-center rounded-b-lg py-10 space-y-5 
             ${item.type === "promo" && "bg-pricing-card-light"}
@@ -15,18 +32,16 @@ export const SubscriptionPlan: FC = () => {
             `}
           >
             <div
-              className={`rounded-md  uppercase font-lexenda_exa font-bold w-[120px] py-1.5 flex justify-center text-sm   ${
-                item.type === "premium"
-                  ? "bg-white text-primary"
-                  : "bg-primary text-white"
-              }`}
+              className={`rounded-md  uppercase font-lexenda_exa font-bold w-[120px] py-1.5 flex justify-center text-sm   ${item.type === "premium"
+                ? "bg-white text-primary"
+                : "bg-primary text-white"
+                }`}
             >
               <span>{item.type}</span>
             </div>
             <span
-              className={`font-extralight font-lexenda_deca text-primary text-6xl ${
-                item.type === "premium" ? "text-white" : "text-primary"
-              }`}
+              className={`font-extralight font-lexenda_deca text-primary text-6xl ${item.type === "premium" ? "text-white" : "text-primary"
+                }`}
             >
               {item.months}
             </span>
